652bb2a2b4380d5c0a169302d0fcf308783bbf25
+ <+>
2023-12-31 16:03:02 -0500

    remove search and tweak css

--- src/index.js
+++ src/index.js
@@ -1,118 +1,89 @@
 
-let currentAlbum;
-// let albums = [];
+let currentAlbum
 
-const albumList = document.querySelector("#album-list");
-const albumDetailImage = document.querySelector("#detail-image");
-const albumDetailTitle = document.querySelector("#title");
-const albumDetailYear = document.querySelector("#year");
-const albumDetailArtist = document.querySelector("#artist");
+const albumList = document.querySelector("#album-list")
+const albumImage = document.querySelector("#detail-image")
+const albumTitle = document.querySelector("#title")
+const albumYear = document.querySelector("#year")
+const albumArtist = document.querySelector("#artist")
 
-const reviewsList = document.querySelector('#reviews-list');
-
-// const searchInput = document.getElementById('search-input')
+const reviewsList = document.querySelector('#reviews-list')
 
 fetch("http://localhost:3000/albums")
     .then(response => response.json())
     .then(albumData => {
         albumData.map(eachAlbum => {
-            createImageMenu(eachAlbum);
-        });
+            createImageMenu(eachAlbum)
+        })
 
-        displayAlbumDetail(albumData[0]);
+        displayAlbumDetail(albumData[0])
 
-        makeNewReview();
-    });
+        makeNewReview()
 
-// rendering functions
+    })
+
+
 function createImageMenu(album) {
-    const image = document.createElement("img");
-    image.src = album.image;
-    albumList.appendChild(image);
+    const image = document.createElement("img")
+    image.src = album.image
+    albumList.appendChild(image)
 
     image.addEventListener("click", () => {
-        displayAlbumDetail(album);
+        displayAlbumDetail(album)
     });
 }
 
 const displayAlbumDetail = (album) => {
-    currentAlbum = album;
+    currentAlbum = album
 
-    albumDetailImage.src = album.image;
-    albumDetailTitle.textContent = album.title;
-    albumDetailYear.textContent = album.year;
-    albumDetailArtist.textContent = album.artist;
+    albumImage.src = album.image
+    albumTitle.textContent = album.title
+    albumYear.textContent = album.year
+    albumArtist.textContent = album.artist
 
     if (album.reviews && album.reviews.length > 0) {
-        displayReviews(album.reviews);
+        displayReviews(album.reviews)
     } else {
-        reviewsList.innerHTML = '';
+        reviewsList.innerHTML = ''
     }
 }
 
 function displayReviews(reviews) {
-    reviewsList.innerHTML = '';
-    reviews.forEach(getReview);
+    reviews.forEach(getReview)
 }
 
 function getReview(review) {
-    const list = document.createElement('ul');
-    list.textContent = review.content;
-    reviewsList.appendChild(list);
+    const list = document.createElement('ul')
+    list.textContent = review.content
+    reviewsList.appendChild(list)
 }
 
 function makeNewReview() {
     document.querySelector('#review-form').addEventListener("submit", (event) => {
-        event.preventDefault();
-        const reviewForm = event.target;
+        event.preventDefault()
+        const reviewForm = event.target
 
-        const newReviewDescription = document.querySelector('#review');
-        const newReview = document.createElement('ul');
-        newReview.textContent = newReviewDescription.value;
+        const newReviewDescription = document.querySelector('#review')
+        const newReview = document.createElement('ul')
+        newReview.textContent = newReviewDescription.value
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.classList.add('delete-button'); 
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'Delete'
+        deleteButton.classList.add('delete-button')
         deleteButton.addEventListener('click', () => {
-           newReview.remove();
+           newReview.remove()
         });
         
-        newReview.appendChild(deleteButton);
+        newReview.appendChild(deleteButton)
 
-        newReview.appendChild(deleteButton);
-        addNewReview(newReview);
+        newReview.appendChild(deleteButton)
+        addNewReview(newReview)
 
-        reviewForm.reset();
+        reviewForm.reset()
     });
 }
 
 function addNewReview(newReview) {
-    reviewsList.appendChild(newReview);
+    reviewsList.appendChild(newReview)
 }
 
-
-
-// searchInput.addEventListener('input', function () {
-//     const searchTerm = searchInput.value.toLowerCase();
-//     const filteredAlbums = albums.filter(album => album.title.toLowerCase().includes(searchTerm));
-
-    
-//     albumList.innerHTML = '';
-
-  
-//     filteredAlbums.forEach(eachAlbum => {
-//         createImageMenu(eachAlbum);
-//     });
-
-    
-//     if (filteredAlbums.length > 0) {
-//         displayAlbumDetail(filteredAlbums[0]);
-//     } else {
-      
-//         albumDetailImage.src = '';
-//         albumDetailTitle.textContent = 'No matching albums';
-//         albumDetailYear.textContent = '';
-//         albumDetailArtist.textContent = '';
-//         reviewsList.innerHTML = '';
-//     }
-// });
