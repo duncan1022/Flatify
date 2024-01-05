d823d93c478c0b31d9d3d4ca3d8ddbffef8740a4
ckorley4 <ckorley4uo@yahoo.com>
2024-01-04 21:22:39 -0500

    Adding the search button

--- src/index.js
+++ src/index.js
@@ -1,89 +1,85 @@
-
 let currentAlbum
 
-const albumList = document.querySelector("#album-list")
-const albumImage = document.querySelector("#detail-image")
-const albumTitle = document.querySelector("#title")
-const albumYear = document.querySelector("#year")
-const albumArtist = document.querySelector("#artist")
+const albumList = document.querySelector('#album-list')
+const albumImage = document.querySelector('#detail-image')
+const albumTitle = document.querySelector('#title')
+const albumYear = document.querySelector('#year')
+const albumArtist = document.querySelector('#artist')
 
 const reviewsList = document.querySelector('#reviews-list')
 
-fetch("http://localhost:3000/albums")
-    .then(response => response.json())
-    .then(albumData => {
-        albumData.map(eachAlbum => {
-            createImageMenu(eachAlbum)
-        })
-
-        displayAlbumDetail(albumData[0])
-
-        makeNewReview()
-
+fetch('http://localhost:3000/albums')
+  .then((response) => response.json())
+  .then((albumData) => {
+    albumData.map((eachAlbum) => {
+      createImageMenu(eachAlbum)
     })
 
+    displayAlbumDetail(albumData[0])
+
+    makeNewReview()
+  })
 
 function createImageMenu(album) {
-    const image = document.createElement("img")
-    image.src = album.image
-    albumList.appendChild(image)
+  const image = document.createElement('img')
+  image.src = album.image
+  albumList.appendChild(image)
 
-    image.addEventListener("click", () => {
-        displayAlbumDetail(album)
-    });
+  image.addEventListener('click', () => {
+    displayAlbumDetail(album)
+  })
 }
 
 const displayAlbumDetail = (album) => {
-    currentAlbum = album
+  currentAlbum = album
 
-    albumImage.src = album.image
-    albumTitle.textContent = album.title
-    albumYear.textContent = album.year
-    albumArtist.textContent = album.artist
+  albumImage.src = album.image
+  albumTitle.textContent = album.title
+  albumYear.textContent = album.year
+  albumArtist.textContent = album.artist
 
-    if (album.reviews && album.reviews.length > 0) {
-        displayReviews(album.reviews)
-    } else {
-        reviewsList.innerHTML = ''
-    }
+  if (album.reviews && album.reviews.length > 0) {
+    displayReviews(album.reviews)
+  } else {
+    reviewsList.innerHTML = ''
+  }
 }
 
 function displayReviews(reviews) {
-    reviews.forEach(getReview)
+  reviews.forEach(getReview)
 }
 
 function getReview(review) {
-    const list = document.createElement('ul')
-    list.textContent = review.content
-    reviewsList.appendChild(list)
+  const list = document.createElement('ul')
+  list.textContent = review.content
+  reviewsList.appendChild(list)
 }
 
 function makeNewReview() {
-    document.querySelector('#review-form').addEventListener("submit", (event) => {
-        event.preventDefault()
-        const reviewForm = event.target
+  document.querySelector('#review-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    const reviewForm = event.target
 
-        const newReviewDescription = document.querySelector('#review')
-        const newReview = document.createElement('ul')
-        newReview.textContent = newReviewDescription.value
+    const newReviewDescription = document.querySelector('#review')
+    const newReview = document.createElement('ul')
+    newReview.textContent = newReviewDescription.value
 
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent = 'Delete'
-        deleteButton.classList.add('delete-button')
-        deleteButton.addEventListener('click', () => {
-           newReview.remove()
-        });
-        
-        newReview.appendChild(deleteButton)
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    deleteButton.classList.add('delete-button')
+    deleteButton.addEventListener('click', () => {
+      newReview.remove()
+    })
 
-        newReview.appendChild(deleteButton)
-        addNewReview(newReview)
+    newReview.appendChild(deleteButton)
 
-        reviewForm.reset()
-    });
+    newReview.appendChild(deleteButton)
+    addNewReview(newReview)
+
+    reviewForm.reset()
+  })
 }
 
 function addNewReview(newReview) {
-    reviewsList.appendChild(newReview)
+  reviewsList.appendChild(newReview)
 }
-
